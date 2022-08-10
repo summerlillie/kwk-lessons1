cb26357acb57b3e317acf0c8cd043c2a26b1ee43
summerlillie <110996638+summerlillie@users.noreply.github.com>
2022-08-10 11:37:39 -0400

    updated with folder

---
+++ summersproject/summersproject/summersproject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  summersproject
+//
+//  Created by Scholar on 8/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("This is a print statement - testing testing!")
+    }
+
+
+}
+
